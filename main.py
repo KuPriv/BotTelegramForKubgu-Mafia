414b49ec4ab881f012a4aaddeb13a30244e1c7e6
Egor Sezen <sezen.egor@mail.ru>
2024-07-21 23:54:05 +0300

    i am tired

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import logging
 from aiogram import Bot, Dispatcher
 from aiogram.enums.parse_mode import ParseMode
 from aiogram.fsm.storage.memory import MemoryStorage
-from idea.config import token
+from config import token
 
 
 async def main():

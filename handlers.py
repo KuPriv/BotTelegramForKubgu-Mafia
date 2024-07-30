d15904735b55b8c9503e395341ccea48ce1a969f
Egor Sezen <sezen.egor@mail.ru>
2024-07-30 11:49:49 +0300

    added gif(dont use), some handlers for ban/unban/greeting and remove handlers from main.py to handlers.py

--- handlers.py
+++ handlers.py
@@ -1,6 +1,115 @@
 import asyncio
-from aiogram import types, Router
-from aiogram.types import Message
-from aiogram.filters import Command
 
+from aiogram import Bot, types, Router
+from aiogram.client.default import DefaultBotProperties
+from aiogram.enums.parse_mode import ParseMode
+from aiogram.filters import Command
+from aiogram.filters.chat_member_updated import \
+    ChatMemberUpdatedFilter, IS_NOT_MEMBER, MEMBER
+from aiogram.types import FSInputFile, ChatMemberBanned, ChatMemberRestricted
+from config_for_test import token
+from config import my_id, chat_id
+
+
+bot = Bot(token, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
 router = Router()
+
+
+@router.chat_join_request()
+async def handle_join_request(message: types.Message):
+    try:
+        print("dp.chat_join_request()")
+        user_status = await bot.get_chat_member(chat_id=message.chat.id, user_id=message.from_user.id)
+        print(user_status)
+        if isinstance(user_status, ChatMemberBanned) or isinstance(user_status, ChatMemberRestricted):
+            print('i am called here (unban) in join request')
+            try:
+                await bot.restrict_chat_member(
+                    chat_id=message.chat.id,
+                    user_id=message.from_user.id,
+                    permissions=types.ChatPermissions(
+                        can_send_messages=True,
+                        can_send_media_messages=True,
+                        can_send_polls=True,
+                        can_send_other_messages=True,
+                        can_add_web_page_previews=True,
+                        can_invite_users=True,
+                        can_pin_messages=True
+                    )
+                )
+            except Exception as e:
+                await message.reply(f"Произошла ошибка: {e}")
+        print(f"comeback israil {message.from_user.username}")
+        await bot.approve_chat_join_request(chat_id=message.chat.id, user_id=message.from_user.id)
+        #gif = FSInputFile("greeting.gif", filename="greeting.gif")
+        await asyncio.sleep(2)
+        await bot.send_message(chat_id=message.chat.id, text=f'Приветствуем сектанта: <a href="tg://user?{message.from_user.id}">{message.from_user.username}</a>')
+        #await bot.send_animation(chat_id=message.chat.id, animation=gif)
+    except Exception as e:
+        await message.reply(f"Произошла ошибка: {e}")
+
+
+@router.chat_member()
+async def handle_invite_request(message: types.Message):
+    print("dp.chat_member()")
+    user_status = await bot.get_chat_member(chat_id=message.chat.id, user_id=message.from_user.id)
+    print(user_status)
+    if isinstance(user_status, ChatMemberBanned):
+        print('i am called here (unban)')
+        try:
+            await bot.restrict_chat_member(
+                chat_id=message.chat.id,
+                user_id=message.from_user.id,
+                permissions=types.ChatPermissions(
+                    can_send_messages=True,
+                    can_send_media_messages=True,
+                    can_send_polls=True,
+                    can_send_other_messages=True,
+                    can_add_web_page_previews=True,
+                    can_invite_users=True,
+                    can_pin_messages=True
+                )
+            )
+        except Exception as e:
+            await message.reply(f"Произошла ошибка: {e}")
+        try:
+            await bot.send_message(message.from_user.id, f'<a href="https://t.me/mafiaKUBGU">В пизду я бы не заходил сюда</a> ', parse_mode='HTML')
+        except Exception as e:
+            await message.reply(f"Произошла ошибка: {e}")
+
+
+@router.message(Command("bye"))
+async def ban_me(message: types.Message):
+    await bot.ban_chat_member(chat_id=message.chat.id, user_id=my_id)
+    print("Handled command 'bye'")
+    """await bot.restrict_chat_member(
+        chat_id=chat_id,
+        user_id=my_id,
+        permissions=types.ChatPermissions(
+            can_send_messages=True,
+            can_send_media_messages=True,
+            can_send_polls=True,
+            can_send_other_messages=True,
+            can_add_web_page_previews=True,
+            can_invite_users=True,
+            can_pin_messages=True
+        )
+    )"""
+
+
+@router.message(Command("hi"))
+async def unban_user(message: types.Message):
+    print("Handled command 'hi'")
+    await bot.restrict_chat_member(
+        chat_id=chat_id,
+        user_id=my_id,
+        permissions=types.ChatPermissions(
+            can_send_messages=True,
+            can_send_media_messages=True,
+            can_send_polls=True,
+            can_send_other_messages=True,
+            can_add_web_page_previews=True,
+            can_invite_users=True,
+            can_pin_messages=True
+        )
+    )
--- main.py
+++ main.py
@@ -1,58 +1,20 @@
 import asyncio
 import logging
-import re
 
-from aiogram import Bot, Dispatcher, types
+from aiogram import Bot, Dispatcher
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums.parse_mode import ParseMode
-from aiogram.filters import Command
 from aiogram.fsm.storage.memory import MemoryStorage
-from pyrogram import Client
+from config_for_test import token
+import handlers
 
-from config_for_test import token, api_hash, api_id
-bot = Bot(token, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
-dp = Dispatcher(storage=MemoryStorage())
-
-#game@MafiososBot
-"""доделать вызов гея который начал игру
-и еще сделать таймер на 5 минут чтоб тегать раз в 5 минут людей"""
-@dp.message(Command("nasrek"))
-async def get_user_ids(message: types.Message):
-    chat = await bot.get_chat(message.chat.id)
-    if chat.type in ["supergroup", "group"]:
-        s = []
-        async with Client("my_account", api_id, api_hash, bot_token=token) as app:
-            async for m in app.get_chat_members(message.chat.id):
-                s.append(m)
-        id_list = re.findall(r'id=\d{9}', str(s))
-        #usernames_list = re.findall(r'username=[^\s) ]*', str(s))
-        id_list = set(id_list)
-        id_list = list(id_list)
-        counter = 0
-        counter_for_users = 0
-        s = ""
-        for i in id_list:
-            if counter == 5:
-                await bot.send_message(message.chat.id, s, parse_mode='HTML')
-                s = ''
-                counter = 0
-            mention = f'<a href="tg://user?{str(i)}">{counter_for_users}</a> '
-            s += mention
-            print(s)
-            counter += 1
-            counter_for_users += 1
-    if chat.type not in ["supergroup", "group"]:
-        result = await bot.delete_message(chat_id=message.chat.id, message_id=message.message_id)
-    result = await bot.get_chat_member_count(chat_id=message.chat.id)
-
-
-"""@dp.message(Command("bb"))
-async def get_user_ids(message: types.Message):
-    await message.answer('я спать всем пока')
-"""
 
 async def main():
-    #dp.message.register(cmd_end, Command("end"))
+    bot = Bot(token, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+    dp = Dispatcher(storage=MemoryStorage())
+    dp.include_router(handlers.router)
+
+    await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
 
 

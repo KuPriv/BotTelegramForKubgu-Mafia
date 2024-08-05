61f1f9331537d5a281beaa0d96304ffed4f49f9b
Egor Sezen <sezen.egor@mail.ru>
2024-08-05 12:09:49 +0300

    Revert "some_changes"
    This reverts commit b64ce8df5d9a2194a4a4773d091a9d29224ad330.

--- main.py
+++ main.py
@@ -6,18 +6,19 @@ from aiogram.client.default import DefaultBotProperties
 from aiogram.enums.parse_mode import ParseMode
 from aiogram.fsm.storage.memory import MemoryStorage
 from config_for_test import token
-import handlers, hadlers_for_ls
+import handlers
 
 
 async def main():
     bot = Bot(token, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
     dp = Dispatcher(storage=MemoryStorage())
-    dp.include_routers(handlers.router, hadlers_for_ls.router)
+    dp.include_router(handlers.router)
 
     await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
 
 
 if __name__ == "__main__":
+    # Включаем логирование, чтобы не пропустить важные сообщения
     logging.basicConfig(level=logging.INFO)
     asyncio.run(main())

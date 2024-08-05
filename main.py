573ba54889a56f7e649a94e962929df5723e2282
Egor Sezen <sezen.egor@mail.ru>
2024-08-05 12:37:28 +0300

    fixed_broken...

--- main.py
+++ main.py
@@ -6,19 +6,18 @@ from aiogram.client.default import DefaultBotProperties
 from aiogram.enums.parse_mode import ParseMode
 from aiogram.fsm.storage.memory import MemoryStorage
 from config_for_test import token
-import handlers
+import handlers, hadlers_for_ls
 
 
 async def main():
     bot = Bot(token, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
     dp = Dispatcher(storage=MemoryStorage())
-    dp.include_router(handlers.router)
+    dp.include_routers(handlers.router, hadlers_for_ls.router)
 
     await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
 
 
 if __name__ == "__main__":
-    # Включаем логирование, чтобы не пропустить важные сообщения
     logging.basicConfig(level=logging.INFO)
     asyncio.run(main())
